133d8fd3d05c285043ce8c908b6aead16bd04698
stekc <me@stkc.win>
2024-02-21 23:02:23 -0800

    Add basic commands, cache Canister requests

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ import urllib
 
 import aiohttp
 import asyncpraw
+from aiocache import cached
 from dotenv import load_dotenv
 
 load_dotenv()
 
+footer = "\n\n^I ^am ^a ^bot. ^Powered ^by ^[Canister](https://canister.me). ^Written ^by ^[stkc](https://stkc.win). ^Use ^!help ^to ^see ^commands. ^Beep ^boop, ^etc."
 
+
+@cached(ttl=86400)
+async def canister_fetch_repos():
+    async with aiohttp.ClientSession() as client:
+        async with client.get(
+            "https://api.canister.me/v2/jailbreak/repository/ranking?rank=*"
+        ) as resp:
+            if resp.status == 200:
+                response = await resp.json(content_type=None)
+                return response.get("data")
+        return None
+
+
+@cached(ttl=86400)
 async def get_packages_from_canister(query: str):
     ignored_repos = ["zodttd", "modmyi"]
     async with aiohttp.ClientSession() as client:
@@ -38,6 +54,7 @@ async def get_packages_from_canister(query: str):
                 return None
 
 
+@cached(ttl=3600)
 async def format_package_info(package):
     if not package["name"]:
         package["name"] = package["package"]
@@ -56,23 +73,62 @@ async def process_comment(comment):
     pattern = re.compile(
         r".*?(?<!\[)+\[\[((?!\s+)([\w+\ \&\+\-\<\>\#\:\;\%\(\)]){2,})\]\](?!\])+.*"
     )
+    command_pattern = r".*!(\w+)(?:\s+(\w+))?$"
     try:
         if comment.author.name in [os.getenv("USERNAME"), "AutoModerator"]:
             return
 
+        # Command handling
+        match = re.search(command_pattern, comment.body.lower().strip())
+        if match:
+            command = match.group(1)
+            subquery = match.group(2)
+            if command == "repo":
+                if subquery:
+                    print(
+                        f"!repo {subquery} ran by u/{comment.author.name} \nhttps://reddit.com{comment.permalink}\n"
+                    )
+                    repos = await canister_fetch_repos()
+                    query = comment.body.lower().strip().split(" ")[1]
+                    matches = [
+                        repo
+                        for repo in repos
+                        if repo.get("slug")
+                        and repo.get("slug") is not None
+                        and repo.get("slug").lower() == subquery.lower()
+                    ]
+                    repo_data = matches[0]
+                    name = repo_data.get("name")
+                    url = repo_data.get("uri")
+                    description = repo_data.get("description")
+                    if name and url:
+                        await comment.reply(
+                            f"[{name}]({url}) [[Add Repo](https://repos.slim.rocks/repo/?repoUrl={url})]\n\n    {description}"
+                            + footer
+                        )
+            if command == "help":
+                print(
+                    f"!help ran by u/{comment.author.name} \nhttps://reddit.com{comment.permalink}\n"
+                )
+                await comment.reply(
+                    f"Commands:\n\n!repo <repo slug> (!repo bigboss){footer}"
+                )
+                return
+
+        # Package search
         if match := pattern.match(comment.body):
             query = match.group(1)
             if len(query) < 3:
                 return
             print(
-                f"Comment from u/{comment.author.name} matched ({query})\nhttps://reddit.com{comment.permalink}\n"
+                f"Package from u/{comment.author.name} matched ({query})\nhttps://reddit.com{comment.permalink}\n"
             )
 
             if packages := await get_packages_from_canister(query):
                 response = "\n".join(
                     [await format_package_info(package) for package in packages]
                 )
-                response += "\n\n^I ^am ^a ^bot. ^Powered ^by ^[Canister](https://canister.me). ^Written ^by ^[stkc](https://stkc.win). ^Beep ^boop, ^etc."
+                response += footer
                 await comment.reply(response)
 
     except Exception as e:
